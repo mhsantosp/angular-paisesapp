f3593e128f56d5828945a37d41ee10f080045d30
Maria Helena Santos Pineda <maria.santos@sophossolutions.com>
2021-07-08 23:17:04 -0500

    Seccion 8: SPA - App Paises

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais !: Country;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) { }
+
+  ngOnInit(): void {
+    // this.activateRoute.params
+    //   .subscribe(({ id }) => {
+    //     console.log(id);
+
+    //     this.paisService.getPaisPorAlpha(id)
+    //       .subscribe(pais => {
+    //         console.log(pais);
+    //       });
+    //   });
+
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe(pais => this.pais = pais);
+  }
+
+}
