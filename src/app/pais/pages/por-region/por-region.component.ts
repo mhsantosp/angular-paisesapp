ed434814457ea3d33ec0fbfc70e479d3c4cff654
Maria Helena Santos Pineda <maria.santos@sophossolutions.com>
2021-07-10 00:22:43 -0500

    Final seccion 9 app Paises

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+      button {
+        margin-right: 8px;
+      }
+    `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regActiva: string = '';
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region: string): string {
+    return (region === this.regActiva) ? 'btn btn-info' : 'btn btn-outline-info';
+  }
+
+  activarRegion(region: string) {
+    if (region === this.regActiva) { return; }
+
+    this.regActiva = region;
+    this.paises = [];
+
+    //TODO: llamar el servicio
+    this.paisService.buscarRegion(region)
+      .subscribe(paises => this.paises = paises);
   }
 
 }
